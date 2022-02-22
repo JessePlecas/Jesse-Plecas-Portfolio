4c970dae6d31763ffd19a938d734fb13dfa2f9e0
JessePlecas <jesseplecas@gmail.com>
2022-02-22 18:01:51 +0000

    Add about page

--- contact-form-handler.php
+++ contact-form-handler.php
@@ -9,13 +9,16 @@
         $time = time();
 
         $to ="jesse@jesseplecas.com";
+        $headers  = 'MIME-Version: 1.0' . "\r\n";
         $headers = "From: ".$email;
         $txt = "You have a new email from ".$name.".\n\n".$message;
+
+        echo $message;
 
         mail($to, $subject, $txt, $headers);
         header("Location: contact.html?mailsend");
 
         print "Script Ran $time";
-       
+       echo ("thank you");
     }
 ?>
